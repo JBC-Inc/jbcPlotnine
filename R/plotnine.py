99cae23540f8e7a4308ea78d20f0d515cd606dfa
Jeff Craggy <craggyjeff@gmail.com>
2024-05-22 16:57:38 -0500

    code fix

--- R/plotnine.py
+++ R/plotnine.py
@@ -20,17 +20,7 @@ from datetime import datetime, timezone
 # import warnings
 # warnings.filterwarnings("ignore")
 
-# dir(plotnine.themes)
-# ['__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', 
-# '__name__', '__package__', '__path__', '__spec__', 'elements', 'targets', 
-# 'theme', 'theme_538', 'theme_bw', 'theme_classic', 'theme_dark', 'theme_get', 
-# 'theme_gray', 'theme_grey', 'theme_light', 'theme_linedraw', 
-# 'theme_matplotlib', 'theme_minimal', 'theme_seaborn', 'theme_set', 
-# 'theme_tufte', 'theme_update', 'theme_void', 'theme_xkcd', 'themeable']
-# 
-# theme_set(theme_classic(base_size=12, base_family='Times New Roman'))
-
-#==============================================================================
+#=================== 2024 PLOTNINE CONTEST - POSIT ============================
 # Rules:
 # 
 #  1.Technically impressive
@@ -50,10 +40,6 @@ qual = pd.read_csv('./inst/data/qual.csv',
                    dtype={'region': 'string', 
                           'Historic': 'string', 
                           'Forecast': 'string'})
-                          
-reference_date = qual['date'].min()
-qual['days_since_ref'] = (qual['date'] - reference_date).dt.days
-
 clr = 'firebrick'
 
 fcst_hs = datetime(2009, 4, 1, tzinfo=timezone.utc)   # Forecast Historic Start April 1, 2009
@@ -76,7 +62,7 @@ def log_breaks(x):
     cycles = np.arange(lower, upper + 1, 1)
     return np.power(10, cycles)
 
-def log_minor_breaks(x):
+# def log_minor_breaks(x):
     # """
     # Calculate logarithmic minor breaks based on major breaks.
     # 
@@ -122,85 +108,122 @@ def label_comma(x, digits=0):
     """
     return [f"{value:,.{digits}f}" for value in np.asarray(x)]
 
-def date_to_days(date):
-    return (date - start_d).days
-
 def p9(qual, metric1, metric2, quality, log):
-
-  plt.close('all')
+    """
+    Create plotnine plot.
   
-  p = (
-    ggplot()
+    Parameters:
+      qual (pd.DataFrame): Approximately 10 years of historical natural gas well
+        production data including: 
+        rate time decline curves
+        cumulative possible estimated ultimate recovery
+        cumulative probable estimated ultimate recovery
+        assessed quality of forecasts made on the data set
+      metric1 (string): Observed historical production rate ('rate', 'cum')
+      metric2 (string): Predicted or Probable production rate ('rate_hat', 
+                        'cum_hat_fit', 'cum_hat_pred')
+      quality (boolean): Whether to plot a graph representing the quality of the
+                         forecast that accompanies historical production over
+                         time. Default True.
+      log (boolean): Whether to plot production in actual or log scale. Default
+                     False.
+    Returns:
+      Side effect is to draw a plotnine object to the `Plots` pane in R Studio.
+    """
+    plt.close('all')
   
-    # Historic ----------------------------------------------------------------
-    + geom_point(data=qual,
-                 mapping=aes('date', metric1, shape='Historic'),
-                 color='firebrick',
-                 fill='None',
-                 size=2.4,
-                 stroke=0.42)
+    p = (
+      ggplot()
+  
+      # Historic --------------------------------------------------------------
+    
+      + geom_point(data=qual,
+                   mapping=aes('date', metric1, shape='Historic'), 
+                   color='firebrick',
+                   fill='None',
+                   size=2.4,
+                   stroke=0.42)
                    
-    # Forecast ----------------------------------------------------------------
-    + geom_line(data=qual,
-                mapping=aes('date', metric2, group='Forecast', color='Forecast'))
+      # Forecast --------------------------------------------------------------
+    
+      + geom_line(data=qual,
+                  mapping=aes('date', metric2, group='Forecast', color='Forecast'))
+                
+      # Scales, Themes, Labels ------------------------------------------------
 
-    + scale_x_datetime(date_breaks='1 year', 
-                       date_labels="%Y",
-                            limits=[fcst_hs - pd.DateOffset(months=6), 
-                                    hist_he + pd.DateOffset(months=6)],
-                            expand=(0, 0))
+      + scale_x_datetime(date_breaks='1 year', 
+                         date_labels="%Y",
+                              limits=[fcst_hs - pd.DateOffset(months=6), 
+                                      hist_he + pd.DateOffset(months=6)],
+                              expand=(0, 0))
     
-    + theme(plot_margin_top=0.05)
+      + theme(plot_margin_top=0.05)
     
-    + labs(x='', y='')
-  )
+      + labs(x='', y='')
+    )
   
-  if log == True:
-    p = p + scale_y_log10(breaks=log_breaks,
+    # Log Scale ---------------------------------------------------------------
+  
+    if log == True:
+      p = p + scale_y_log10(breaks=log_breaks,
                             limits=log_limits,
                             labels=label_comma)
-  else:
-    p = p + scale_y_continuous(labels = label_comma)
-
-  return p
+    else:
+      p = p + scale_y_continuous(labels = label_comma)
+      
+    return p
 
 def plot_nine(qual, metric1, metric2, quality=False, log=False):
-
+  """
+  Draw a secondary axis on top of existing plotnine figure. The function will
+  first call p9 which generates the actual plotnine object (or draws it), then
+  matplotlib.pyplot will be used to create a second x axis on top. The axis
+  will display the days elapsed since production started.
+  
+  Because this feature is not yet implemented into plotnine functionality,
+  (i.e. scale_x_datetime(sec.axis)), this function emulates the desired output.
+  
+  Parameters are inherited from p9()
+  
+  Returns:
+    Side effect is to draw a plotnine graph to the `Plots` pane, then a 
+    secondary x axis containing the 'days' scale.
+  """
   # plotnine.ggplot
   p9(qual, metric1, metric2, quality, log).show()
   
+  # Secondary Axis hack -------------------------------------------------------
   # matplotlib.pyplot
   plt.plot([], [])
 
   # new axis 
   ax = plt.gca()
 
-  # overlay secondary axis on plotnine plot
+  # overlay secondary axis on plotnine plot, match limits of ggplot
   sec = ax.twiny()
   sec.set_xlim(ax.get_xlim());
   sec.set_ylim(ax.get_ylim());
   
   # Historic/Forecast start
-  start_date = qual['date'].iloc[0]
-  tick_locations = [start_date]
+  tick_locations = [fcst_hs]
   tick_labels = [0]  # Days since reference for start_date
 
   # Add tick locations for January 1st of subsequent years
   for year in range(2010, 2020):
     january_1st = datetime(year, 1, 1, tzinfo=timezone.utc)
     tick_locations.append(january_1st)
-    tick_labels.append((january_1st - start_date).days)
+    tick_labels.append((january_1st - fcst_hs).days)
   
   # Set ticks and labels
   sec.set_xticks(ticks=tick_locations, labels=tick_labels)
 
-  # Hide spines
+  # Hide secondary spines
   sec.spines['top'].set_visible(False)
   sec.spines['right'].set_visible(False)
   sec.spines['bottom'].set_visible(False)
   sec.spines['left'].set_visible(False)
 
-  # Hide x minor ticks
+  # Hide ax minor ticks
   ax.tick_params(axis='x', which='minor', color='white')
   
   plt.show()
@@ -211,23 +234,55 @@ def plot_nine(qual, metric1, metric2, quality=False, log=False):
 
 # Historical rate time decline curve with forecasted production of a gas well.
 
+# Rate Time decline curve
 plot_nine(qual, 'rate', 'rate_hat', log=False)
-
-
-
-
 plot_nine(qual, 'rate', 'rate_hat', log=True)
 
-plot_nine(qual, 'cum', 'cum_hat_fit', log=False).show()
-plot_nine(qual, 'cum', 'cum_hat_fit', log=True).show()
+# Cumulative estimate production
+plot_nine(qual, 'cum', 'cum_hat_fit', log=False)
+plot_nine(qual, 'cum', 'cum_hat_fit', log=True)
 
-plot_nine(qual, 'cum', 'cum_hat_predict', log=False).show()
-plot_nine(qual, 'cum', 'cum_hat_predict', log=True).show()
+# Cumulative predicted production
+plot_nine(qual, 'cum', 'cum_hat_pred', log=False)
+plot_nine(qual, 'cum', 'cum_hat_pred', log=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #==============================================================================
 #==============================================================================
 #==============================================================================
+
+# dir(plotnine.themes)
+# ['__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', 
+# '__name__', '__package__', '__path__', '__spec__', 'elements', 'targets', 
+# 'theme', 'theme_538', 'theme_bw', 'theme_classic', 'theme_dark', 'theme_get', 
+# 'theme_gray', 'theme_grey', 'theme_light', 'theme_linedraw', 
+# 'theme_matplotlib', 'theme_minimal', 'theme_seaborn', 'theme_set', 
+# 'theme_tufte', 'theme_update', 'theme_void', 'theme_xkcd', 'themeable']
+# 
+# theme_set(theme_classic(base_size=12, base_family='Times New Roman'))
+
+
+
+
 
 
 # scales are named according to the type of variable they align with: 
@@ -255,183 +310,6 @@ plot_nine(qual, 'cum', 'cum_hat_predict', log=True).show()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# plotnine secondary x axis with matplotlib pyplot method ==================
-
-# [1] - 1 figure exists on the pyplot stack. 
-#       everytime "p" is defined, another figure is created
-#
-# [1, 2, 3, 4, etc...]
-
-plt.close('all')
-plt.get_fignums()
-
-# this creates a figure
-p = (ggplot() 
-     + geom_point(aes(x=[1, 2, 3], y=[4, 5, 6]), color = 'purple', size = 5)
-     
-     # make room for second x axis
-     + theme(plot_margin_top=0.05))
-
-# this also creates a figure
-# p.show()
-
-plt.get_fignums()
-
-# this creates a plot on the current figure, or creates a figure
-plt.plot([], [])
-
-plt.get_fignums()
-
-# create new MATPLOTLIB axis for existing figure plot
-ax1 = plt.gca()
-
-# set a seconday axis
-sec = ax1.twiny()
-
-# match the limits to the original
-sec.set_xlim(ax1.get_xlim())
-sec.set_ylim(ax1.get_ylim())
-
-# remove the borders
-sec.spines['top'].set_visible(False)
-sec.spines['right'].set_visible(False)
-sec.spines['bottom'].set_visible(False)
-sec.spines['left'].set_visible(False)
-
-# this does NOT create a figure
-plt.show()
-
-
-# the problem now becomes: how to shrink ggplot for top x axis? 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# create new axes for plt, which is part of fig 1 plt
-ax1 = plt.gca()
-
-plt.get_fignums() # [1]
-
-# create a secondary X axis on top
-secondary_x = ax1.twiny()
-
-# lookat the x, y limits
-ax1.get_ylim()
-ax1.get_xlim()
-
-# set the same limits for the secondary axis
-secondary_x.set_xlim(ax1.get_xlim())
-secondary_x.set_ylim(ax1.get_ylim())
-
-# make optional label with position 
-# secondary_x.set_xlabel("AAAAAA", x=0.4, y=0)
-
-plt.show()
-
-
-
-
 # plt.gca()
 #======================================================================================
 # if there is no axes on this figure (but we havent created a figure yet, only a plot?)
@@ -449,88 +327,6 @@ plt.close()           # close a figure window
 
 plt.get_fignums()
 fig.axes
-ax.clear
-ax.cla
-ax
-
-ax = plt.gca()
-ax.clear()
-fig.axes
-
-
-
-
-
-ax = plt.gca()
-
-ax.twiny()
-
-ax.set_xlabel("secondary x")
-
-#plt.subplots_adjust(hspace=10)
-
-plt.show()
-
-
-
-
-plt.plot([1, 2, 3, 4], [10, 20, 25, 30])
-plt.xlabel('Primary X-axis')
-plt.ylabel('Y-axis')
-
-# Create a secondary x-axis on top
-ax1 = plt.gca()
-ax2 = ax1.twiny()
-
-# Set the same limits for the secondary x-axis
-ax2.set_xlim(ax1.get_xlim())
-
-# Set the labels for the secondary x-axis
-ax2.set_xlabel('Secondary X-axis')
-
-# Optionally customize the secondary x-axis ticks and labels
-ax2.set_xticks([1, 2, 3, 4])
-ax2.set_xticklabels(['A', 'B', 'C', 'D'])
-
-# Adjust the layout to shorten the height of the actual plot area
-plt.subplots_adjust(bottom=0.15)  # Decrease the bottom margin
-
-# Show the plot
-plt.show()
-
-
-
-
-
-
-# plt.twinx()
-#==============================================================================
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Create some mock data
-t = np.arange(0.01, 10.0, 0.01)
-data1 = np.exp(t)
-data2 = np.sin(2 * np.pi * t)
-
-fig, ax1 = plt.subplots()
-
-color = 'tab:red'
-ax1.set_xlabel('time (s)')
-ax1.set_ylabel('exp', color=color)
-ax1.plot(t, data1, color=color)
-ax1.tick_params(axis='y', labelcolor=color)
-
-ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
-
-color = 'tab:blue'
-ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
-ax2.plot(t, data2, color=color)
-ax2.tick_params(axis='y', labelcolor=color)
-
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
-plt.show()
 
 
 
